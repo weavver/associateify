845fe57f0622fafdb81ab0fa2db64e000085c072
Mitchel Constantin <mythicalbox@weavver.com>
2013-04-20 00:39:10 -0700

    initial commit of the associateify library

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 associateify
 ============
 
-Automatically add your affiliate id to Amazon.com links on your website
+Automatically adds your affiliate id to Amazon.com links on your website
+
+To use add jquery to your website (if you don't already have it):
+     <script type="text/javascript" src="jquery-1.9.1.min.js"></script>
+
+And then add the associateify.js file:
+
+<script type="text/javascript" src="associateify.js"></script>
+
+
+If you have ajax or other events that load content on your site you may want to run "tagLinks()" from in your load complete events.
+
+
+One idea on how to use this is to add it to your website forum to automatically tag posted Amazon links.
+ 
+Please consider leaving the "tip" percent above 0 for using this code.. :)
+
+
+If you want help or support feel free to e-mail me:
+
+Mitchel Constantin
+mythicalbox@weavver.com
+Weavver, Inc.
---
+++ src/associateify.js
@@ -0,0 +1,32 @@
+
+var associateId = "youraffiliateTag";
+var tipPercent = 5; // 5% of links
+
+$(document).ready(function () {
+     tagLinks();
+});
+
+function tagLinks() {
+     var tipId = "weainc-20";
+
+     $matchedlinks = $('a')
+          .filter(function () {
+               return $(this).attr('href').match(/(http|https):\/\/www.amazon.com/);
+          });
+
+     $matchedlinks.each(function () {
+          var rndNum = Math.floor((Math.random() * 100) + 1);
+          var tagId = (rndNum <= tipPercent) ? tipId : associateId;
+          var href = $(this).attr('href');
+          // to be polite we check that nobody else tagged amazon before us, that we we don't "steal" their referral
+          if (!href.match(/tag=/)) {
+               if (href.match(/\?/)) {
+                    href = href + "&tag=" + tagId;
+               }
+               else {
+                    href = href + "?tag=" + tagId;
+               }
+               $(this).attr('href', href);
+          }
+     });
+}
